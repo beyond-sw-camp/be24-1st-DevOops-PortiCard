3d0618a233034bbadd6d3e2ec65b1a15556a6ae4
NahunE <myjsh6547@naver.com>
2026-01-19 09:35:51 +0900

    [Remove] 사용하지 않는 html 제거

--- front/assets/js/footer.js
+++
@@ -1,7 +0,0 @@
-// footer.html을 가져와서 footer-placeholder에 넣기
-fetch('components/footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.querySelector('#footer-placeholder').innerHTML = data;
-    })
-    .catch(error => console.error('푸터 로드 실패:', error));
